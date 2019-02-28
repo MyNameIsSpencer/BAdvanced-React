c320a1b19973b56e95269f73ec542061ef1866f6
spencer kyonka <spencer.kyonka@hotmail.ca>
2019-02-28 00:05:39 -0500

    first commit

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,17 @@
+// import React from 'react';     // <<< next.js will take care of importing React for us
+
+// class Home extends React.Component {
+//   render () {
+//     return <h1> Hey! </h1>
+//   }
+// }
+
+import Link from 'next/link';
+
+const Home =  props => (
+  <div>
+    <h1> Hey!! </h1>
+  </div>
+);
+
+export default Home
