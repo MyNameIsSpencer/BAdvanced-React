5205f918164a86ea70c1e5b434d18e9cddd6446a
spencer kyonka <spencer.kyonka@hotmail.ca>
2019-03-22 16:32:22 -0400

    search bar with autocomplete

--- sick-fits/frontend/.next/static/runtime/webpack.js
+++ sick-fits/frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4cc118646c3e975777c9";
+/******/ 	var hotCurrentHash = "bea649fc8318b5150ed3";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
