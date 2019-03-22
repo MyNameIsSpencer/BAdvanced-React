2e6ada40cce2ecb063641b1d5afd0ca843167697
spencer kyonka <spencer.kyonka@hotmail.ca>
2019-03-22 03:25:01 -0400

    update cart after delete

--- sick-fits/frontend/.next/static/runtime/webpack.js
+++ sick-fits/frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "eada28ae0bba75165f4a";
+/******/ 	var hotCurrentHash = "4cc118646c3e975777c9";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
